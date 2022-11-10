a62936f5d63977f2578bd5b0ce9bc262c6b6b6c6
CoderRedwing <ajiteshru@gmail.com>
2022-11-10 23:41:01 +0530

    commit for the master branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
- import 'pages/home_page.dart';
+import 'package:flutter_app/utils/myroutes.dart';
+import 'pages/home_page.dart';
 import 'pages/login_page.dart';
- void main (){
-  runApp(MyApp());
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
 }
-  class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext Context) {
-      return MaterialApp(
-        themeMode: ThemeMode.light,
-        theme: ThemeData(primaryShgfghwatch: Colors.deepOrange),
-        darkTheme: ThemeData(
-          brightness: Brightness.dark,
-        ),
-        initialRoute: "/home",
-        routes: {
-          "/": (context) => LoginPage(),
-          "/home" : (context) =>HomePage(),
-          "/Login": (context) => LoginPage()
-        },
-      );
-    }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => const LoginPage(),
+        MyRoutes.homeRoute: (context) => const HomePage(),
+        MyRoutes.loginRoute: (context) => const LoginPage()
+      },
+    );
   }
+}
